8f56d800ca5b6920ab7b15cccd7e8898580eaea8
jgrt <asati.jagrati93@gmail.com>
2020-10-23 23:51:55 +0530

    project setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="code_20201021_jagratiasati", # Replace with your own username
+    version="0.0.1",
+    author="Jagrati Asati",
+    author_email="asati.jagrati93@gmail.com",
+    description="Body Mass Index Calculator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=['pandas>=1.0.0'],
+    extra_require={
+        "dev": [
+            "pytest"
+        ]
+    }
+)
