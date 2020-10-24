f2dd7fd3f4e6d974c5ab564a5ca349dc2eed82e2
jgrt <asati.jagrati93@gmail.com>
2020-10-24 23:58:15 +0530

    Add package data

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="code_20201021_jagratiasati", # Replace with your own username
+    name="code_20201021_jagratiasati",  # Replace with your own username
     version="0.0.1",
     author="Jagrati Asati",
     author_email="asati.jagrati93@gmail.com",
@@ -12,11 +12,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    python_requires='>=3.6',
-    install_requires=['pandas>=1.0.0'],
-    extra_require={
-        "dev": [
-            "pytest"
-        ]
-    }
+    include_package_data=True,
+    entry_points={"console_scripts": ["calculator = calculator.main:main"]},
+    python_requires=">=3.6",
+    install_requires=["pandas>=1.0.0"],
+    extra_require={"dev": ["pytest"]},
 )
